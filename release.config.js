0aec62db88f79418742a49e7f66f80b85ca22f0b
DelStar <delenet@yahoo.com>
2023-07-31 22:25:01 -0400

    update

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-   branches: "main",
-   repositoryUrl: "https://github.com/Kenmakhanu/actionstest.git",
+   branches: "master",
+   repositoryUrl: "https://github.com/DelStar/actionstest",
    plugins: [
      '@semantic-release/commit-analyzer',
      '@semantic-release/release-notes-generator',
