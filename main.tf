acf60a300d5656c0ede16cb69413c3c2d3280e17
DelStar <delenet@yahoo.com>
2023-08-01 17:20:44 -0400

    fix: SemVar

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-2"
 }
 
 resource "aws_vpc" "actions" {
   cidr_block = "10.0.0.0/24"
 
   tags = {
-    Name        = "Class30"
+    Name        = "Class31"
     Team        = "DevOps"
     Environment = "Prod"
   }
